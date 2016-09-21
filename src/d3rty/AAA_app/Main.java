72dd722d9a36132d953efc4f86e541665a5a0b99
Polkovnik0ff <polkovnikandrew@gmail.com>
2016-09-21 20:04:24 +0700

    added class User

--- src/d3rty/AAA_app/Main.java
+++ src/d3rty/AAA_app/Main.java
@@ -4,7 +4,7 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello, World!");
-        System.out.println("hello!!!!!!!!");
+        User A = new User(1, "John Doe", "jdoe", "sup3rpaZZ");
+
     }
 }
---
+++ src/d3rty/AAA_app/User.java
@@ -0,0 +1,21 @@
+package d3rty.AAA_app;
+
+/**
+ * Created by Andrew on 21.09.2016.
+ */
+
+class User {
+    public  int id;
+    public String name;
+    public String login;
+    public String password;
+
+    public User(int id,String name,String login,String password){
+
+        this.id = id;
+        this.name = name;
+        this.login = login;
+        this.password = password;
+
+    }
+}
