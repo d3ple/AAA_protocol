f7ad5f4408261855ee715d78578d328a74f95a0b
Polkovnik0ff <polkovnikandrew@gmail.com>
2016-09-24 11:48:17 +0700

    added class Role

--- src/d3rty/AAA_app/Main.java
+++ src/d3rty/AAA_app/Main.java
@@ -1,10 +1,18 @@
 package d3rty.AAA_app;
+import java.util.ArrayList;
 
-public class Main
-{
-    public static void main(String[] args)
-    {
-        User A = new User(1, "John Doe", "jdoe", "sup3rpaZZ");
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<User> users = new ArrayList<>();
+        users.add(new User(1, "John Doe", "jdoe", "sup3rpaZZ"));
+        users.add(new User(2, "Jane Row", "jrow", "Qweqrty12"));
 
-    }
-}
+
+        ArrayList<Role> roles = new ArrayList<>();
+        roles.add(new Role(1, users.get(0), "READ", "a"));
+        roles.add(new Role(2, users.get(0), "WRITE", "a.b"));
+        roles.add(new Role(3, users.get(1), "EXECUTE", "a.b.c"));
+        roles.add(new Role(4, users.get(0), "EXECUTE", "a.bc"));
+
+
+    }}
