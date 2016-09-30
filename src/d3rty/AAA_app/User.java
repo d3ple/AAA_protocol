dd6a154d6f00e39314257321ee33575c25c35686
Polkovnik0ff <polkovnikandrew@gmail.com>
2016-09-30 19:51:21 +0700

    added class Security

--- src/d3rty/AAA_app/User.java
+++ src/d3rty/AAA_app/User.java
@@ -1,4 +1,7 @@
 package d3rty.AAA_app;
+import  static d3rty.AAA_app.Security.Salt;
+import  static d3rty.AAA_app.Security.MD5;
+
 
 
 class User {
@@ -6,13 +9,15 @@ class User {
     public String name;
     public String login;
     public String password;
+    public String Salt;
 
     public User(int id,String name,String login,String password){
 
         this.id = id;
         this.name = name;
         this.login = login;
-        this.password = password;
+        Salt = Salt();
+        this.password =  MD5(MD5(password) + Salt);
 
     }
 }
