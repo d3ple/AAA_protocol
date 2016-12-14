d771481f17b1363214f26c2eadc724853950bcca
D3rty <d.danil.d2@gmail.com>
2016-12-14 16:10:16 +0700

    Testing fixes

--- src/test/java/com/d3rty/aaaprotocol/CheckingTest.java
+++ src/test/java/com/d3rty/aaaprotocol/CheckingTest.java
@@ -1,21 +1,16 @@
 package com.d3rty.aaaprotocol;
 
-import com.d3rty.aaaprotocol.dao.DbManager;
 import com.d3rty.aaaprotocol.dao.DbUserSelecting;
 import com.d3rty.aaaprotocol.domain.User;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-
-import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class CheckingTest {
-    private static User curUser;
+    private static User thisUser;
 
     @Before
     public void setUp() throws Exception {
@@ -27,16 +22,12 @@ public class CheckingTest {
 
     }
 
-    @Test
-    public void checkLogin() throws Exception {
-
-//        DbManager conn = mock(DbManager.class);
-//        conn.connect();
-
-//        DbUserSelecting usersel = mock(DbUserSelecting.class);
-//        when(usersel.getUserByLogin("login")).thenReturn(curUser =
+//    @Test
+//    public void checkLogin() throws Exception {
+//        DbUserSelecting dbu = mock(DbUserSelecting.class);
+//        when(dbu.getUserByLogin("login")).thenReturn(thisUser =
 //                new User(1, "name", "login", "password", "salt"));
-    }
+//    }
 
     @Test
     public void checkPassword() throws Exception {
